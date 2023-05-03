78485b201a03280f2a74287f5cbe268b73716b82
Archit Jha <103206051+architjha15@users.noreply.github.com>
2023-05-03 23:38:53 +0530

    Add files via upload

---
+++ IfElse.java
@@ -0,0 +1,14 @@
+// Program to find the number is odd or even
+import java.util.*;
+class IfElse {
+    public static void main(String[] args) {
+        Scanner store = new Scanner(System.in);
+        System.out.print("Conditional Statements:\n" + "Enter any number: ");
+        int num = store.nextInt();
+        if (num % 2 == 0) {
+            System.out.println(num + " is an even number.");
+        } else {
+            System.out.println("Neither odd nor even.");
+        }
+    }
+}
