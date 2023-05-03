dcce497cccca0b0166b6d0b730be7bcef0e40fee
Archit Jha <103206051+architjha15@users.noreply.github.com>
2023-05-03 23:39:59 +0530

    Update IfElse.java

--- IfElse.java
+++ IfElse.java
@@ -5,7 +5,7 @@ class IfElse {
         Scanner store = new Scanner(System.in);
         System.out.print("Conditional Statements:\n" + "Enter any number: ");
         int num = store.nextInt();
-        if (num % 2 == 0) {
+        if (num % 2 == 0) {     // This is a condition
             System.out.println(num + " is an even number.");
         } else {
             System.out.println("Neither odd nor even.");
